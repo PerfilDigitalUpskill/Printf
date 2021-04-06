e722dba56444df94205d153b13882ec970e27da5
Manuel Almeida <malmeida@c1r12s6.42lisboa.com>
2021-04-06 16:24:50 +0100

    Struct is now receiving values, but precision and the dot seems to have fucked up something, check it tomorrow

--- ft_printf.c
+++ ft_printf.c
@@ -6,95 +6,128 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 12:08:38 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/01 15:09:03 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/06 16:23:48 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			get_flags(char *str, int *i, t_flags flags)
+void			get_flags(char *str, int *i, t_flags *flags)
 {
 	int				z;
 
 	z = *i;
-	while (str[z] && (str[z] == '0' || str(z) == '-'))
+	while (str[z] && (str[z] == '0' || str[z] == '-'))
 	{
 		if (str[z] == '0')
-			flags.zero = 1;
+			flags->zero = 1;
 		if (str[z] == '-')
-			flags.minus = 1;
+			flags->minus = 1;
 		z++;
 	}
 	*i = z;
 }
 
-void			get_width(char *str, int *i, va_list args, t_flags flags)
+void			get_width(char *str, int *i, va_list args, t_flags *flags)
 {
 	int				z;
+	int				nb;
 
+	nb = 0;
 	z = *i;
-	while (str[z] && ((str[z] => '0' && str[z] <= '9') || str[z] == '*'))
+	while (str[z] && ((str[z] >= '0' && str[z] <= '9') || str[z] == '*'))
 	{
-		if (str[z] == '*')
-		{		
-			flags.width = va_arg(args, int);
-			z++;
-		}
 		while (str[z] >= '0' && str[z] <= '9')
 		{
-			flags.width *= 10 + (str[z] - '0');
+			nb = nb * 10 + (str[z] - '0');
+			z++;
+		}
+		if (nb != 0)
+			flags->width = nb;
+		if (str[z] == '*')
+		{		
+			flags->width = va_arg(args, int);
 			z++;
 		}
 	}
 	*i = z;
 }
 
-void			get_conversion(char *str, int *i, t_flags flags)
+void			get_conversion(char *str, int *i, t_flags *flags)
 {
 	int			z;
 
 	z = *i;
 	if (str[z] == 'c')
-		flags.conversion = 'c';
+		flags->conversion = 'c';
 	else if (str[z] == 's')
-		flags.conversion = 's';	
+		flags->conversion = 's';	
 	else if (str[z] == 'p')
-		flags.conversion = 'p';
+		flags->conversion = 'p';
 	if (str[z] == 'd')
-		flags.conversion = 'd';
+		flags->conversion = 'd';
 	else if (str[z] == 'i')
-		flags.conversion = 'i';	
+		flags->conversion = 'i';	
 	else if (str[z] == 'u')
-		flags.conversion = 'u';
+		flags->conversion = 'u';
 	else if (str[z] == 'x')
-		flags.conversion = 'x';	
+		flags->conversion = 'x';	
 	else if (str[z] == 'X')
-		flags.conversion = 'X';
+		flags->conversion = 'X';
 	else
-		printf("fucked up");
+		printf("fucked up\n");
 	*i = z;
 }
 
-int				conversion_parser(char *str, va_list args, int i)
+void			get_precision(char *str, int *i, va_list args, t_flags *flags)
 {
-	t_flags			flags;
-
-	flags = initializeflags;
-	i += 1;
-	get_flags();
-	get_width();
-	get_conversion();
+	int			z;
+	int			nb;
 	
-	conversion_placer;
+	z = *i;
+	nb = 0;
+	if (str[z] == '.')
+	{
+		z++;
+		if (str[z] == '*')
+			flags->precision = va_arg(args, int);
+		else
+		{
+		   while (str[z] >= '0' && str[z] <= '9')
+		   {
+			nb = nb * 10 + (str[z] - '0');
+			z++;
+		   }
+		   flags->precision = nb;
+		}
+	}
+	*i = z;
 }
 
-t_flags			initializeflags(void)
+t_flags			init_flags(void)
 {
 	t_flags			flags;
 
 	flags.zero = 0;
 	flags.minus = 0;
-	flag.width = 0;
+	flags.width = 0;
+	flags.precision = -1;
+	return (flags);
+}
+
+int				conversion_parser(char *str, va_list args, int i)
+{
+	t_flags		flags;
+
+	flags  = init_flags();
+	i += 1;
+	get_flags(str, &i, &flags);
+	get_width(str, &i, args, &flags);
+	get_conversion(str, &i, &flags);
+
+	printf("zero is %d, minus is %d, width is %d, precision is %d, conversion is %c\n", flags.zero, flags.minus, flags.width, flags.precision, flags.conversion); 
+
+	return (i);	
 }
 
 int				parse_str(char *str, va_list args)
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 12:13:18 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/01 15:09:05 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/06 16:22:11 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ typedef struct	s_flags
 	int			zero;
 	int			minus;
 	int			width;
-	int			dot;
 	int			precision;
-	int 		precision_star;
 	char		conversion;
 }				t_flags;
 
@@ -36,7 +34,7 @@ char	*ft_strdup(const char *s1);
 void	ft_putnbr(int nb);
 void	ft_putnbr_base(int nb, char *base);
 int		ft_atoi(const char *nptr);
-int		parse_str(const char *str, va_list args);
+int		parse_str(char *str, va_list args);
 int		ft_printf(const char *, ...);
 
 #endif
