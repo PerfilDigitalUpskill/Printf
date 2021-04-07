5633a8314cadbdd73c609657a601d98274c45de4
Manuel Almeida <malmeida@c1r11s6.42lisboa.com>
2021-04-07 15:49:11 +0100

    Main file for testing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 14:53:42 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/07 15:29:36 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/07 15:47:52 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,13 @@ int		main()
 	ft_printf("%-6s\n", "Bfica");
 	ft_printf("%-10s\n", "Bfica");
 	ft_printf("%-20s\n", "Bfica");
+	ft_printf("__________________\n");
+	ft_printf("%.2s\n", "Bfica");
+	ft_printf("%.4s\n", "Bfica");
+	ft_printf("%6.4s\n", "Bfica");
+	ft_printf("%-6.4s\n", "Bfica");
+	ft_printf("%8.2s\n", "Bfica");
+	ft_printf("%8.2s\n", "Bfica");
 	ft_printf("__________________\n");
 
 	return (0);
