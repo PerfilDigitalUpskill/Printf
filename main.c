84c8654af304a56b8947bee45e6f677ba458e4bb
TrincaMelros <malmeida@student.42lisboa.com>
2021-04-09 15:26:30 +0100

    🧐 testing different cases 🧐

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 14:53:42 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/07 16:14:39 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/09 14:27:07 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int		main()
 {
+/*	
 	ft_printf("%s\n", "Bfica");
 	ft_printf("%4s\n", "Bfica");
 	ft_printf("%5s\n", "Bfica");
@@ -35,6 +36,12 @@ int		main()
 	ft_printf("%8.2s\n", "Bfica");
 	ft_printf("%8.2s\n", "Bfica");
 	ft_printf("__________________\n");
-
+	ft_printf("%c\n", 'f');
+	ft_printf("%4c\n", 'f');
+	ft_printf("__________________\n");
+	ft_printf("%d\n", 22);
+*/
+	ft_printf("%.4d\n", 22);
+	ft_printf("%.10d\n", 22);
 	return (0);
 }
