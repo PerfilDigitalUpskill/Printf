6dc2c2d075f2f12c0c5751b6655675433f0619e8
TrincaMelros <malmeida@student.42lisboa.com>
2021-04-10 15:28:01 +0100

    😤 testing errythang (not really) 😤

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 14:53:42 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/09 16:57:21 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/10 15:27:12 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		main()
 {
-	
+/*
 	ft_printf("%s\n", "Bfica");
 	ft_printf("%4s\n", "Bfica");
 	ft_printf("%5s\n", "Bfica");
@@ -39,17 +39,25 @@ int		main()
 	ft_printf("%c\n", 'f');
 	ft_printf("%4c\n", 'f');
 	ft_printf("__________________\n");
-	ft_printf("%d\n", 22);
-	ft_printf("%4d\n", 22);
-	ft_printf("%-4d\n", 22);
-	ft_printf("%06d\n", 22);
-	printf("%d\n", 22);
-	printf("%4d\n", 22);
-	printf("%-4d\n", 22);
-	printf("%06d\n", 22);
-/*
+	ft_printf("%u\n", 22);
+	ft_printf("%4.6u\n", 22);
+	ft_printf("%-4.8u\n", 22);
+	ft_printf("%06u\n", 22);
+	printf("%u\n", 22);
+	printf("%4.6u\n", 22);
+	printf("%-4.8u\n", 22);
+	printf("%06u\n", 22);
+	ft_printf("_________________\n");
 	ft_printf("%8d\n", 22);
 	ft_printf("%-8.4d\n", 22);
 */
+	ft_printf("%x\n", 34);
+	printf("%x\n", 34);
+	ft_printf("%8x\n", 34);
+	printf("%8x\n", 34);
+	ft_printf("%.4X\n", 34);
+	printf("%.4X\n", 34);
+	ft_printf("%-4X\n", 34);
+	printf("%-4X\n", 34);
 	return (0);
 }
