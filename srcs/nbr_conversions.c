76575c0e7f0797b155b199870494821b3697e477
TrincaMelros <malmeida@student.42lisboa.com>
2021-04-14 15:41:09 +0100

    Fixed some int conversion edge cases

--- srcs/nbr_conversions.c
+++ srcs/nbr_conversions.c
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 13:28:50 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/14 14:05:10 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/14 15:40:27 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,18 @@ void	signed_int_conversion(va_list args, t_flags *flags)
 	if (flags->precision > len)
 		nbr = apply_precision(nbr, len, flags);
 	len = ft_strlen(nbr);
-	while (flags->width > len++ && !(flags->minus) && !(flags->zero))
+	while (flags->width > len && !(flags->minus) && !(flags->zero))
+	{
 		ft_putchar(' ');
-	while (flags->width > len++ && flags->zero)
+		len++;
+	}
+	while (flags->width > len && flags->zero)
+	{
 		ft_putchar('0');
-	ft_putstr(nbr);
+		len++;
+	}
+	if (!(flags->precision == 0 && nbr[0] == '0' && nbr[1] == '\0'))
+		ft_putstr(nbr);
 	while (flags->width > len++ && flags->minus)
 		ft_putchar(' ');
 	freebird((void *)&nbr);
