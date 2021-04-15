5750ccbc737fc00c5a2d4e2d22f902f7a527a166
TrincaMelros <malmeida@student.42lisboa.com>
2021-04-15 17:47:58 +0100

    yes yes YES

--- srcs/nbr_conversions.c
+++ srcs/nbr_conversions.c
@@ -6,7 +6,7 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 13:28:50 by malmeida          #+#    #+#             */
-/*   Updated: 2021/04/15 17:30:24 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/04/15 17:43:26 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,33 +84,40 @@ void	signed_int_conversion(va_list args, t_flags *flags)
 
 void	unsigned_int_conversion(va_list args, t_flags *flags)
 {
-	char	*nbr;
-	int		len;
+	char					*nbr;
+	int						len;
+	long unsigned int		k;
 
+	k = va_arg(args, unsigned int);
+	nbr = ft_itoa(k);
+	if (nbr[0] == '0' && nbr[1] == '\0' && flags->precision == 0)
+	{
+		freebird((void *)&nbr);
+		nbr = ft_strdup("");
+	}	
+	len = ft_strlen(nbr);
 	flags_checker(flags);
-	nbr = ft_itoa(va_arg(args, unsigned int));
-	len = ft_strlen(nbr);
-	if (flags->precision > len)
-		nbr = apply_precision(nbr, len, flags);
-	len = ft_strlen(nbr);
-	while (flags->width > len && !(flags->minus) && !(flags->zero))
-	{
-		ft_putchar(' ');
-		len++;
-	}
-	while (flags->width > len && flags->zero)
-	{
-		ft_putchar('0');
-		len++;
-	}
-	ft_putstr(nbr);
-	while (flags->width > len && flags->minus)
-	{
-		ft_putchar(' ');
-		len++;
-	}
+	handle_conversions(flags, &nbr, len);
+	freebird((void *)&nbr);
 }
 
+void	hex_low_conversion(va_list args, t_flags *flags)
+{
+	char					*nbr;
+	int						len;
+
+	nbr = hex_itoa(va_arg(args, unsigned int), "0123456789abcdef");
+	if (nbr[0] == '0' && nbr[1] == '\0' && flags->precision == 0)
+	{
+		freebird((void *)&nbr);
+		nbr = ft_strdup("");
+	}	
+	len = ft_strlen(nbr);
+	flags_checker(flags);
+	handle_conversions(flags, &nbr, len);
+	freebird((void *)&nbr);
+}
+/*
 void	hex_low_conversion(va_list args, t_flags *flags)
 {
 	char	*nbr;
@@ -138,8 +145,9 @@ void	hex_low_conversion(va_list args, t_flags *flags)
 		ft_putchar(' ');
 		len++;
 	}
+	freebird((void *)&nbr);
 }
-
+*/
 void	hex_up_conversion(va_list args, t_flags *flags)
 {
 	char	*nbr;
@@ -167,4 +175,5 @@ void	hex_up_conversion(va_list args, t_flags *flags)
 		ft_putchar(' ');
 		len++;
 	}
+	freebird((void *)&nbr);
 }
